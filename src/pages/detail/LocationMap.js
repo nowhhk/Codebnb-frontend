fe171e689b448b991408b96afc9161e3c72d8658
kenneth8128 <kenkim@younggap.co>
2020-06-19 12:03:25 +0900

    google map 좌표 data 받기 구현

--- src/pages/detail/LocationMap.js
+++ src/pages/detail/LocationMap.js
@@ -1,68 +1,72 @@
-import React from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { API } from '../../config'
-
-// const LocationMap = () => {
-//   const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(`${API}/room/detail/1`)
-  //     .then((res) => res.json())
-  //     // .then((res) => console.log(res));
-  //     .then((res) => {
-  //       setData(res.room_info);
-  //     });
-  // }, []);
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { API } from "../../config";
+import { withRouter } from "react-router-dom";
 
 const containerStyle = {
-  width: '100%',
-  height: '500px'
+  width: "100%",
+  height: "500px",
 };
- 
-const center = {
-  lat: 33.518140,
-  lng: 126.523240
-};
- 
+
+// const center = {
+//   lat: 33.51814,
+//   lng: 126.52324,
+// };
+
 function LocationMap() {
-  const [map, setMap] = React.useState(null)
- 
+  const [map, setMap] = React.useState(null);
+  const [data, setData] = useState({});
+
+  // fetch(`${API}/room/detail/${this.props.match.params.id}`)
+
+  useEffect(() => {
+    fetch(`${API}/room/detail/1`)
+      .then((res) => res.json())
+      // .then((res) => console.log(res));
+      .then((res) => {
+        setData(res.room_info);
+      });
+  }, []);
+
+  // const center = {
+  //   lat: 33.51814,
+  //   lng: 126.52324,
+  // };
+
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
- 
+    setMap(map);
+  }, []);
+
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
- 
-
-
-
+    setMap(null);
+  }, []);
 
   return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyCqJz2U6vsxMjz3npYEj1mBqvzrPpVbgYY"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={13}
-        // onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        <Marker
-          position={center}
-        />
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  )
+    <>
+      {data.latitude && (
+        <LoadScript googleMapsApiKey="AIzaSyCqJz2U6vsxMjz3npYEj1mBqvzrPpVbgYY">
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={{ lat: Number(data.latitude), lng: Number(data.longitude) }}
+            zoom={13}
+            // onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            <Marker
+              position={{
+                lat: Number(data.latitude),
+                lng: Number(data.longitude),
+              }}
+            />
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
+      )}
+    </>
+  );
 }
- 
 
-
-export default LocationMap;
-
+export default withRouter(LocationMap);
